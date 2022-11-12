3d1011e574ad9153e14b6e49b7e58cf0ab9a5f36
Kishaloy Neogi <nkishaloy@yahoo.com>
2022-11-12 22:05:41 +0100

    Version 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct MdFile<'a> {
+    pub name: &'a str,
+    pub content: String,
+}
+
+impl MdFile<'_> {
+    pub fn add<T: std::fmt::Display>(&mut self, pre: &str, post: &str, dig: usize, val: T) -> T {
+        self.content
+            .push_str(&format!("{pre} {val:.dig$} {post}\n\n"));
+        val
+    }
+
+    pub fn push_str(&mut self, s: &str) {
+        self.content.push_str(s);
+    }
+}
+
+impl Drop for MdFile<'_> {
+    fn drop(&mut self) {
+        std::fs::write(self.name, &self.content).unwrap();
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
 }
